17737e686c7afae5a2a64391051a17b8bd97f2c1
copaiement <cole.paiement@gmail.com>
2023-12-06 12:41:01 -0800

    Update routes and pull full messages from DB

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date(),
+const router = express.Router();
+
+// query DB for messages
+
+const Message = require('../models/message');
+
+const messages = [];
+
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const cursor = Message.find().sort({ added: -1 }).cursor();
+
+// MongoDB access URL (hide this later?)
+
+const mongoDB = 'mongodb+srv://MessageAdmin:vttq3Ci9GQU4Decz@cluster0.qz7yuhz.mongodb.net/message-board?retryWrites=true&w=majority';
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  console.log('Debug: About to connect');
+  await mongoose.connect(mongoDB);
+  console.log('Debug: Should be connected?');
+
+  for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+    messages.push(doc);
   }
-];
+
+  console.log('Debug: Closing mongoose');
+  mongoose.connection.close();
+}
+
+// Index Route Main
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
+/* GET new message page listing. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message' });
+});
+
 module.exports = router;
+
