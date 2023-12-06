8b04d30b75e766258d63b00fa9e6fa7001da3623
copaiement <cole.paiement@gmail.com>
2023-12-06 14:17:28 -0800

    Added some style bits

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,40 @@
 const express = require('express');
 
 const router = express.Router();
+const mongoose = require('mongoose');
 
-// query DB for messages
-
+// import schema
 const Message = require('../models/message');
 
-const messages = [];
-
-const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
-const cursor = Message.find().sort({ added: -1 }).cursor();
-
 // MongoDB access URL (hide this later?)
-
 const mongoDB = 'mongodb+srv://MessageAdmin:vttq3Ci9GQU4Decz@cluster0.qz7yuhz.mongodb.net/message-board?retryWrites=true&w=majority';
 
-main().catch((err) => console.log(err));
+// access DB and read messages
+async function load() {
+  console.log('Debug: About to connect');
+  await mongoose.connect(mongoDB);
+  console.log('Debug: Should be connected?');
+  const messages = [];
+  const cursor = Message.find().sort({ added: -1 }).cursor();
+  for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+    messages.push(doc);
+  }
 
-async function main() {
+  console.log('Debug: Closing mongoose');
+  mongoose.connection.close();
+  return messages;
+}
+
+// function to update DB
+async function update(newMsg) {
   console.log('Debug: About to connect');
   await mongoose.connect(mongoDB);
   console.log('Debug: Should be connected?');
 
-  for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
-    messages.push(doc);
-  }
+  await Message.create(newMsg);
 
   console.log('Debug: Closing mongoose');
   mongoose.connection.close();
@@ -36,14 +43,23 @@ async function main() {
 // Index Route Main
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
+  const messages = await load().catch((err) => console.log(err));
   res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
-/* GET new message page listing. */
-router.get('/new', function(req, res, next) {
+/* GET new message page. */
+router.get('/new', (req, res, next) => {
   res.render('form', { title: 'New Message' });
 });
 
-module.exports = router;
+// POST new message
+router.post('/new', async (req, res, next) => {
+  const newMsg = { text: req.body.msg, user: req.body.usr, added: new Date() };
+  // update DB with new message
+  await update(newMsg);
+  // redirect to home
+  res.redirect('/');
+});
 
+module.exports = router;
